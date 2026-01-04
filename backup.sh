589f438dd590f676d7a0f3e73543b46ec14f603a
Colin Heurteaux <dev@heurteaux.me>
2026-01-04 13:04:48 +0100

    fix: reverting back changes

--- backup.sh
+++ backup.sh
@@ -181,16 +181,11 @@ send_pretty_email() {
         exit 1
     fi
 
-    # Substitute large variables (LOGO_IMG, LOGS) using sed, then pass smaller vars to envsubst
-    # This avoids passing huge environment variables to any subprocess
-    if ! cat "$EMAIL_TEMPLATE" | \
-         sed -e "s|\$LOGO_IMG|$LOGO_IMG|g" -e "s|\${LOGO_IMG}|$LOGO_IMG|g" | \
-         sed -e "s|\$LOGS|$LOGS|g" -e "s|\${LOGS}|$LOGS|g" | \
-         (EMAIL_TEMPLATE="$EMAIL_TEMPLATE" LOGO_PATH="$LOGO_PATH" \
-          FORMATTED_HOSTNAME="$FORMATTED_HOSTNAME" OS_NAME="$OS_NAME" BACKUP_TIME="$BACKUP_TIME" \
-          PARAGRAPH="$PARAGRAPH" SUBJECT="$SUBJECT" TITLE="$TITLE" \
-          envsubst) | \
-         fold -s -w 998 | msmtp "$TO_EMAIL"
+    # Pass all variables only to envsubst, not to all subprocesses
+    if ! EMAIL_TEMPLATE="$EMAIL_TEMPLATE" LOGO_PATH="$LOGO_PATH" LOGO_IMG="$LOGO_IMG" \
+         FORMATTED_HOSTNAME="$FORMATTED_HOSTNAME" OS_NAME="$OS_NAME" BACKUP_TIME="$BACKUP_TIME" \
+         PARAGRAPH="$PARAGRAPH" SUBJECT="$SUBJECT" TITLE="$TITLE" LOGS="$LOGS" \
+         cat "$EMAIL_TEMPLATE" | envsubst | fold -s -w 998 | msmtp "$TO_EMAIL"
     then
         error "failed to send alert email"
         exit 1
